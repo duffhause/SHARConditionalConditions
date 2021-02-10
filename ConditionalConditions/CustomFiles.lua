7cf09a4a6920930a4cab2e4d25554d4a25b885f7
duffhause <57455765+duffhause@users.noreply.github.com>
2021-02-10 13:12:08 +0000

    Remove obvious comments

--- ConditionalConditions/CustomFiles.lua
+++ ConditionalConditions/CustomFiles.lua
@@ -1,16 +1,9 @@
--- Process all settings
 Settings = GetSettings(GetModName())
 
-for k,v in pairs(Settings) do
-	print("Setting "..k.." is set to "..tostring(v)..".")
-end
-
--- Method to remove given condition from given file
 function RemoveCondition (file, condition)
 	return file : gsub('AddCondition%s-%(%s-"' .. condition .. '".-%);.-CloseCondition%(%);', '')
 end
 
--- Method to remove instruction from given file
 function RemoveInstruction (file, instruction)
 	return file : gsub(instruction .. '%s-%(.-%)%s-;', '')
 end
