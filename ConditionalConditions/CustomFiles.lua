7709bfad9b51b3876e1f59cd50dec37f5f0ea349
duffhause <57455765+duffhause@users.noreply.github.com>
2021-02-10 13:09:53 +0000

    Revert "Remove obvious comments"
    This reverts commit ee6a569daaa4b79875305ebd28c3a0794d6cad03.

--- ConditionalConditions/CustomFiles.lua
+++ ConditionalConditions/CustomFiles.lua
@@ -1,4 +1,9 @@
+-- Process all settings
 Settings = GetSettings(GetModName())
+
+for k,v in pairs(Settings) do
+	print("Setting "..k.." is set to "..tostring(v)..".")
+end
 
 -- Method to remove given condition from given file
 function RemoveCondition (file, condition)
--- ConditionalConditions/Resources/HandleMission.lua
+++ ConditionalConditions/Resources/HandleMission.lua
@@ -1,6 +1,9 @@
+-- Load the file
 local file = ReadFile("/GameData/" .. GetPath());
 
+--For each condition
 for condition, enabled in pairs(Settings) do
+	-- If the condition is disabled, then remove the condition from the script
 	if not enabled then
 		if condition == "timeout" then
 			file = RemoveInstruction(file, "SetStageTime")
@@ -10,5 +13,6 @@ for condition, enabled in pairs(Settings) do
 	end
 end
 
+--Output modified file
 Output(file)
 	
